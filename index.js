0af9225753d3a2f6c7631b3e65a2fc1c25a6d270
Juan H <juantwothreeinseries@gmail.com>
2022-08-28 22:00:33 -0500

    attempt 4

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use('/api', allRoutes);
 
 app.use(express.static(path.resolve(__dirname, './client/build')));
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
 	res.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
 });
 
