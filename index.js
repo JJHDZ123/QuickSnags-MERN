8a966394ea8202168291bb6e2be5a3ca264ac1d1
Juan H <juantwothreeinseries@gmail.com>
2022-08-28 21:29:20 -0500

    getting ready for deployment

--- index.js
+++ index.js
@@ -18,10 +18,10 @@ app.use(cookieParser());
 
 app.use('/api', allRoutes);
 
-app.use(express.static(path.resolve(__dirname, '../client/build')));
+app.use(express.static(path.resolve(__dirname, './client/build')));
 
 app.get('*', (req, res) => {
-	res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+	res.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
 });
 
 app.use((err, req, res, next) => {
